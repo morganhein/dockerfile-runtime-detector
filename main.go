90452409e45510f6724b0f8f6901fb03e31c9f32
Morgan Hein <morgan@polverse.io>
2019-04-01 09:30:49 -0700

    initial main for imports.

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/fearful-symmetry/garlic"
+
+	"github.com/prometheus/common/log"
+)
+
+func main() {
+	cn, err := garlic.DialPCN()
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+
+	//Read in events
+	for {
+		data, err := cn.ReadPCN()
+
+		if err != nil {
+			log.Errorf("Read fail: %s", err)
+		}
+		fmt.Printf("%#v\n", data)
+	}
 }
